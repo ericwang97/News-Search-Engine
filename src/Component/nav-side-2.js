3381165f093f65be8b49348c0a1242a7dfd75e51
ericwang97 <ericwang97@outlook.com>
2020-06-11 22:33:16 -0700

    Copy project from Gitlab

---
+++ src/Component/nav-side-2.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Link, NavLink} from 'react-router-dom';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined,PieChartOutlined } from '@ant-design/icons';
+import './nav-side-2.css';
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+class NavSide extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="navbar-default navbar-side">
+
+                    <Layout style={{ minHeight: '100vh' }}>
+                        <Sider width={300} className="site-layout-background" >
+                            <Menu
+                                mode="inline"
+                                className="site-layout-background"
+                                defaultSelectedKeys={['search']}
+                                defaultOpenKeys={['world']}
+                                theme="dark"
+                                style={{ height: '150%', borderRight: 0 }}
+                            >
+                                <Menu.Item key="search">
+                                    <PieChartOutlined />
+                                    <Link to='/search'><b>Search</b></Link>
+                                </Menu.Item>
+                                <SubMenu
+                                    key="world"
+                                    title={<span>
+                                                <UserOutlined />
+                                                World
+                                              </span>
+                                    }>
+                                    <Menu.Item key="1"><Link to='/world-city'>City</Link ></Menu.Item>
+                                    <Menu.Item key="2"><Link to='/world-country'>Country</Link ></Menu.Item>
+                                    <Menu.Item key="3"><Link to='/world-countrylanguage'>Country Language</Link ></Menu.Item>
+                                </SubMenu>
+                                <SubMenu
+                                    key="sakila"
+                                    title={
+                                        <span>
+                                            <LaptopOutlined />
+                                            Film
+                                          </span>
+                                    }>
+                                    <Menu.Item key="4"><Link to='/sakila-film'>Film</Link ></Menu.Item>
+                                    <Menu.Item key="5"><Link to='/sakila-actor'>Actor</Link ></Menu.Item>
+                                    <Menu.Item key="6"><Link to='/sakila-film_actor'>Film&Actor</Link ></Menu.Item>
+
+                                    <Menu.Item key="7"><Link to='/sakila-category'>Category</Link ></Menu.Item>
+                                    <Menu.Item key="8"><Link to='/sakila-film_category'>Film&Category</Link ></Menu.Item>
+                                    <Menu.Item key="9"><Link to='/sakila-film_text'>Film Text</Link ></Menu.Item>
+                                    <Menu.Item key="10"><Link to='/sakila-language'>Film Language</Link ></Menu.Item>
+
+                                    <Menu.Item key="11"><Link to='/sakila-address'>Address</Link ></Menu.Item>
+                                    <Menu.Item key="12"><Link to='/sakila-city'>City</Link ></Menu.Item>
+                                    <Menu.Item key="13"><Link to='/sakila-country'>Country</Link ></Menu.Item>
+                                    <Menu.Item key="14"><Link to='/sakila-customer'>Customer</Link ></Menu.Item>
+                                    <Menu.Item key="15"><Link to='/sakila-inventory'>Inventory</Link ></Menu.Item>
+                                    <Menu.Item key="16"><Link to='/sakila-payment'>Payment</Link ></Menu.Item>
+                                    <Menu.Item key="17"><Link to='/sakila-rental'>Rental</Link ></Menu.Item>
+                                    <Menu.Item key="18"><Link to='/sakila-staff'>Staff</Link ></Menu.Item>
+                                    <Menu.Item key="19"><Link to='/sakila-store'>Store</Link ></Menu.Item>
+
+                                </SubMenu>
+                                <SubMenu
+                                    key="customers_order"
+                                    title={
+                                        <span>
+                                            <NotificationOutlined />
+                                            Customers order
+                                          </span>
+                                    }>
+                                    <Menu.Item key="20"><Link to='/customers_order-products'>Products</Link ></Menu.Item>
+                                    <Menu.Item key="21"><Link to='/customers_order-customers'>Customers</Link ></Menu.Item>
+                                    <Menu.Item key="22"><Link to='/customers_order-orderdetails'>Order details</Link ></Menu.Item>
+                                    <Menu.Item key="23"><Link to='/customers_order-orders'>Orders</Link ></Menu.Item>
+                                    <Menu.Item key="24"><Link to='/customers_order-payments'>Payments</Link ></Menu.Item>
+                                    <Menu.Item key="25"><Link to='/customers_order-productlines'>Product lines</Link ></Menu.Item>
+                                    <Menu.Item key="26"><Link to='/customers_order-employees'>Employees</Link ></Menu.Item>
+                                    <Menu.Item key="27"><Link to='/customers_order-offices'>Offices</Link ></Menu.Item>
+                                </SubMenu>
+                            </Menu>
+                        </Sider>
+                    </Layout>
+
+             </div>
+        );
+    }
+}
+
+
+export default NavSide;
---
+++ src/Page/home.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PageTitle from '../Component/page-title.js';
+import './home.css'
+import { Card } from "antd";
+import 'antd/es/card/style/css'; // 加载 CSS
+
+class Home extends React.Component{
+
+
+    render(){
+        return(
+            <div id="page-wrapper">
+                <PageTitle title="INF551 Database Search Engine"/>
+                <div className="row">
+                    <Card title="Some notes" >
+                    <div className="col-md-12">
+                        <h5>Hey! Greeting from Liwei!</h5>
+                        <h5>It's my first time making such an application, thank you for trying this!</h5>
+                        <h5>This is a database search engine.</h5>
+                        <h5>Backend: Python + Flask REST API + MySQL + Firebase</h5>
+                        <h5>Front-end: REACT js + ANT design components</h5>
+                        <div>&nbsp;&nbsp;</div>
+                        <div>I do need a front-end designer to embellish my homepage lol (*╹▽╹*)</div>
+                        <div>So hard to make a full stack application, thank you for the people who helped and supported me.</div>
+                        <div>I did it!!!!!!!</div>
+                        <div>&nbsp;&nbsp;</div>
+                    </div>
+                    </Card>
+                    <div>&nbsp;&nbsp;</div>
+                    <div>&nbsp;&nbsp;</div>
+                    <Card title="How to use" >
+                        <div className="col-md-12">
+                            <h5>Please use Search page to search whatever you want.</h5>
+                            <div>The search results are based on the frequency of your sentence, as well as other features.</div>
+                            <div>Note: I am trying to make it more like a real search engine as much as possible. Further analysis on NLP might be needed.</div>
+                            <div>&nbsp;&nbsp;</div>
+                            <h5>You can also click the table to take a look at the data</h5>
+                            <div>&nbsp;&nbsp;</div>
+                        </div>
+                    </Card>
+
+                </div>
+            </div>
+
+
+        );
+    }
+}
+
+export default Home;
