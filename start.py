453e4e3190f28654245b35c632a14579d5c391d7
ericwang97 <ericwang97@outlook.com>
2020-06-12 18:45:05 -0700

    no message

--- start.py
+++ start.py
@@ -18,12 +18,11 @@ def p2():
 
 if __name__ == "__main__":
 
-# 多线程启动 成功 （一个进程多个线程）
 
-    #pro1 = threading.Thread(target=p1)
-    #pro2 = threading.Thread(target=p2)
+#pro1 = threading.Thread(target=p1)
+#pro2 = threading.Thread(target=p2)
 
-# multiprocessing -- 多进程 相当于多核。要在terminal里跑而不能在IDE的terminal里跑 -- python start.py/ & !!!!!!!!!!!!!!
+# multiprocessing -- python start.py/ & !!!!!!!!!!!!!!
 
     pro1 = multiprocessing.Process(target=p1)
     pro2 = multiprocessing.Process(target=p2)
