02a1c8906d497ece9a0f26720be68f9d3002387b
ericwang97 <ericwang97@outlook.com>
2020-06-14 03:10:38 -0700

    test

--- Backend/get_data.py
+++ Backend/get_data.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     run_schedule = True
 
     if run_schedule:
-        schedule.every(0.5).minutes.do(main)
+        schedule.every(0.2).minutes.do(main)
         #schedule.every().hour.do(main)
         while True:
             schedule.run_pending()
